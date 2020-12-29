4601a90c2f6a4da71b42f19d912ed80448917121
Kai Dong <dongkai981@gmail.com>
2020-12-29 11:05:23 -0600

    Put the sql files in their own foler and fixed some more typos

--- DB/seed.sql
+++ DB/seed.sql
@@ -1,19 +1,19 @@
 -- Departrment seed
-USE employeesDB
+USE employeesDB;
 
 INSERT INTO department (name)
 VALUES ("Sales"),
 ("IT"),
-("Management")
+("Management");
 
 -- Role seed
 INSERT INTO role (title, salary, department_id)
 VALUES ("Account Manager", 50000, 3),
 ("Developer", 100000, 2),
-("Manager", 150000, 1)
+("Manager", 150000, 1);
 
 -- Employee Seed
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Richard", "Nixon", 3, null),
 ("Bob", "Dole", 2, null),
-("George", "Washington", 1, 1),
+("George", "Washington", 1, 1)
