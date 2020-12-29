e3b7071ede7180a69b8321291e8065b4728297f2
Kai Dong <dongkai981@gmail.com>
2020-12-29 10:56:15 -0600

    Updated schema slightly and added seed.sql

--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ USE employeesDB;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
- 
 );
 
 -- Role table
@@ -26,9 +25,8 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  manager_id INT,
   role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INT,
   FOREIGN KEY (manager_id) REFERENCES employee(id)
-
+  FOREIGN KEY (role_id) REFERENCES role(id),
 );
---
+++ seed.sql
@@ -0,0 +1,19 @@
+-- Departrment seed
+USE employeesDB
+
+INSERT INTO department (name)
+VALUES ("Sales"),
+("IT"),
+("Management")
+
+-- Role seed
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Person", 50000, 3),
+("Developer", 100000, 2),
+("Manager", 150000, 1)
+
+-- Employee Seed
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Richard", "Nixon", 3, null),
+("Bob", "Dole", 2, null),
+("George", "Washington", 1, 1),
