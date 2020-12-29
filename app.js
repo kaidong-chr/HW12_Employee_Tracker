acc473cffb4b2f3ef530e54d18e23ed204f26773
Kai Dong <dongkai981@gmail.com>
2020-12-29 11:39:54 -0600

    Added app.js with starter code

---
+++ app.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+let connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employeesDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
