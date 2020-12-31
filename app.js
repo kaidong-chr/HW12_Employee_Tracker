d1401d7d6bb043e25d158a8808a35007342dd96c
Kai Dong <dongkai981@gmail.com>
2020-12-31 12:20:09 -0600

    Added function for the main options

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const logo = require("asciiart-logo");
 
 let connection = mysql.createConnection({
   host: "localhost",
@@ -21,3 +22,41 @@ connection.connect(function(err) {
   runSearch();
 });
 
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+            "Edit Employee Info",
+            "View Roles",
+            "Edit Roles",
+            "View Departments",
+            "Edit Departments"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          employeeSummary();
+          break;
+      case "Edit Employee Info":
+          editEmployees();
+          break;
+      case "View Roles":
+          viewRoles();
+          break;
+      case "Edit Roles":
+          editRoles();
+          break;
+      case "View Departments":
+          viewDepartments();
+          break;
+      case "Edit Departments":
+          editDepartments();
+          break;
+      }
+    });
+}
